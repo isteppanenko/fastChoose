6f9db47be2e462ab51265ae8c20c50a37bfbaf7a
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-01-03 01:48:25 +0100

    edit

--- vote_opera.py
+++ vote_opera.py
@@ -1,12 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.opera.options import Options
+from selenium.webdriver.chrome.options import Options  # Використання ChromeOptions для Opera
 
 def create_opera_driver():
     options = Options()
+    # Додаємо шлях до OperaDriver
     driver_path = r"C:\Users\istep\Downloads\operadriver_win64\operadriver.exe"
-    driver = webdriver.Opera(executable_path=driver_path, options=options)
+    driver = webdriver.Chrome(executable_path=driver_path, options=options)  # Використовуємо ChromeDriver для Opera
     return driver
 
 if __name__ == "__main__":
@@ -16,8 +17,8 @@ if __name__ == "__main__":
     website_url = "https://agroportal.ua/publishing/klub-agroeffektivnosti/vseukrajinskiy-konkurs-sila-agrozmin-onlayn-golosuvannya"
 
     # Локатори
-    person_xpath = "//div[@aria-describedby='OperationDesc_73']"
-    choose_button_xpath = "//button[@class='btn btn-primary' and @data-test='choose-btn']"
+    person_xpath = "//div[@aria-describedby='OperationDesc_73']"  # Вибір Шеховцова Євгена
+    choose_button_xpath = "//button[@class='btn btn-primary' and @data-test='choose-btn']"  # Кнопка "Обрати"
 
     try:
         # Перехід на сайт
